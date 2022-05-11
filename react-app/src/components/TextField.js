47ca294cc9375b700036cba565af535231d774b1
va1erie <valeriebouillon3@gmail.com>
2022-05-11 09:23:50 +0200

    studytype condition: Felder disabled/vorausgefüllt

--- react-app/src/components/TextField.js
+++ react-app/src/components/TextField.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-function TextField({field, id, onChange}) {
+function TextField({field, id, studyType, onChange}) {
+
+  let disabled = false;
+  let value = "";
+
+  if(studyType === 'condition') {
+    disabled = true;
+    if(field.name === 'Alignment ID') {
+      value = id+1;
+    }
+  }
+
   return (
     <div>
         <label> {field.name}
-            <input className='element' type="text" name={(field.name).toLowerCase().replace(' ', '')} id={id} onChange={(e) => onChange(e)}/>
+            <input disabled={disabled} className='element' type="text" name={(field.name).toLowerCase().replace(' ', '')} value={value} id={id} onChange={(e) => onChange(e)}/>
           </label>
     </div>
   )

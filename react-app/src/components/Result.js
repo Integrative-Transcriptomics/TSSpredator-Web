f80202c5210774cd56281d2a95ce7d6a8c0e7187
va1erie <valeriebouillon3@gmail.com>
2022-06-01 15:32:09 +0200

    open new tab

---
+++ react-app/src/components/Result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Result() {
+    return (
+        <div>
+            <header>
+                <h1>TSSpredator</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Result

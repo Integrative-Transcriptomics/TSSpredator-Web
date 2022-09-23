5a2ecd00e3e976bba921779663b2480ce9fd1e8a
M. Witte Paz <mathiaswittepaz@gmail.com>
2022-09-23 14:59:08 +0200

    Last idea of URL change

--- src/components/Main.js
+++ src/components/Main.js
@@ -382,11 +382,7 @@ function Main() {
           // open result in new tab
           //window.open('/result', '_blank', 'noopener,noreferrer');
           let filePath = data.filePath;
-          window.open(
-            `${window.location.hostname}/result/${filePath}`,
-            "_blank",
-            "noopener,noreferrer"
-          );
+          window.open(`result/${filePath}`, "_blank", "noopener,noreferrer");
         } else {
           var error = data.result;
           error = error.split(":")[0] + ":" + error.split(":")[1];

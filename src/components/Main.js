f7465297d8cbecd3218a341135d0ec859135e1f8
M. Witte Paz <mathiaswittepaz@gmail.com>
2022-09-27 16:01:22 +0200

    Commenting out Test Function

--- src/components/Main.js
+++ src/components/Main.js
@@ -400,28 +400,28 @@ function Main() {
       .catch((err) => console.log(err));
   }
 
-  function sendTestResults() {
-    fetch("/api/input-test/", {
-      method: "POST",
-      body: JSON.stringify({ test: "test" }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setLoading([false, false]);
+  // function sendTestResults() {
+  //   fetch("/api/input-test/", {
+  //     method: "POST",
+  //     body: JSON.stringify({ test: "test" }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setLoading([false, false]);
 
-        if (data.result === "success") {
-          // open result in new tab
-          //window.open('/result', '_blank', 'noopener,noreferrer');
-          let filePath = data.filePath;
-          window.open(`result/${filePath}`, "_blank", "noopener,noreferrer");
-        } else {
-          var error = data.result;
-          error = error.split(":")[0] + ":" + error.split(":")[1];
-          showError(error);
-        }
-      })
-      .catch((err) => console.log(err));
-  }
+  //       if (data.result === "success") {
+  //         // open result in new tab
+  //         //window.open('/result', '_blank', 'noopener,noreferrer');
+  //         let filePath = data.filePath;
+  //         window.open(`result/${filePath}`, "_blank", "noopener,noreferrer");
+  //       } else {
+  //         var error = data.result;
+  //         error = error.split(":")[0] + ":" + error.split(":")[1];
+  //         showError(error);
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
 
   /**
    * save uploaded alignment file

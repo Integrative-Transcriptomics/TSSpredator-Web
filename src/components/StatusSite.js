f70c0de81a6fc461651d51d3abdb8a8b200c8776
M. Witte Paz <mathiaswittepaz@gmail.com>
2024-01-19 10:41:13 +0100

    Finalizing async upload

--- src/components/StatusSite.js
+++ src/components/StatusSite.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Error from "./Main/Error";
 import ScrollableTextComponent from "./Result/ScrollableText";
 
